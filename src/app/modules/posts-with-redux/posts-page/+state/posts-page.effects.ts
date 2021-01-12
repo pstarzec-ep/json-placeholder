7bebc9d564847a4aec9fd1909b15580d153a8145
Piotr Starzec <piotr_starzec@epam.com>
2021-01-12 14:55:16 +0100

    add working resolve effects for posts page

--- src/app/modules/posts-with-redux/posts-page/+state/posts-page.effects.ts
+++ src/app/modules/posts-with-redux/posts-page/+state/posts-page.effects.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError, filter, map, switchMap, tap } from 'rxjs/operators';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { ROUTER_NAVIGATED, RouterNavigatedAction } from '@ngrx/router-store';
 import { PostService } from '@app/services';
 import { loadPostsAction, postsLoadedAction, postsLoadFailAction } from './posts-page.actions';
-import { catchError, map, switchMap } from 'rxjs/operators';
-import { of } from 'rxjs';
 
 @Injectable()
 export class PostsPageEffects {
+
+  resolvePosts$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType<RouterNavigatedAction>(ROUTER_NAVIGATED),
+      filter(action => !!action.payload.event.url.match(/^\/posts-with-redux$/)),
+      tap(action => console.log(action)),
+      map(() => loadPostsAction()),
+    );
+  });
 
   loadPosts$ = createEffect(() => this.actions$.pipe(
     ofType<typeof loadPostsAction>(loadPostsAction.type),
