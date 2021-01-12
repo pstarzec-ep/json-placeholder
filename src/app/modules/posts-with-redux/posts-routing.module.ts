b3727f4b576a474756997bf3738aac4df85f59f2
Piotr Starzec <piotr_starzec@epam.com>
2021-01-12 15:40:02 +0100

    fix

--- src/app/modules/posts-with-redux/posts-routing.module.ts
+++ src/app/modules/posts-with-redux/posts-routing.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CommentsResolver } from './comments.resolver';
-import { PostResolver } from './post.resolver';
-import { PostsResolver } from './posts.resolver';
-import { CommentResolver } from './comment.resolver';
 import { PostsPageComponent, PostsPageModule } from './posts-page';
 import { PostPageComponent, PostPageModule } from './post-page';
 import { CommentsComponent, CommentsModule } from './comments';
-import { CommentDetailsComponent } from '@app/modules/posts-with-router/comment-details';
+import { CommentDetailsComponent, CommentDetailsModule } from './comment-details';
 
 const routes: Routes = [
-  { path: '', component: PostsPageComponent, resolve: { posts: PostsResolver } },
+  { path: '', component: PostsPageComponent },
   {
-    path: ':postId', component: PostPageComponent, resolve: { post: PostResolver },
+    path: ':postId', component: PostPageComponent,
     children: [
-      { path: 'comments', component: CommentsComponent, resolve: { comments: CommentsResolver } },
-      { path: 'comment/:commentId', component: CommentDetailsComponent, resolve: { comment: CommentResolver } },
+      { path: 'comments', component: CommentsComponent },
+      { path: 'comment/:commentId', component: CommentDetailsComponent },
     ],
   },
 ];
@@ -26,6 +22,7 @@ const routes: Routes = [
     PostsPageModule,
     PostPageModule,
     CommentsModule,
+    CommentDetailsModule,
   ],
   exports: [RouterModule],
 })
